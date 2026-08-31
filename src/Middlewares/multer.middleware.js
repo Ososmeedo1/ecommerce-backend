5bca4ddd30faf59b7e43622c4abe9e960b8d1180
Ososmeedo1 <153993814+Ososmeedo1@users.noreply.github.com>
2026-08-31 22:50:08 +0300

    define each file size to be maximum 5MB

--- src/Middlewares/multer.middleware.js
+++ src/Middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ import { extensions } from "../Utils/file-extensions.utils.js";
 
 
 export const multerHost = (allowedExtensions = extensions.Images) => {
-  
+
   const storage = multer.diskStorage({});
 
   const fileFilter = (req, file, cb) => {
@@ -16,6 +16,11 @@ export const multerHost = (allowedExtensions = extensions.Images) => {
     cb(new ErrorHandlerClass(`Invalid file type, only ${allowedExtensions} images are allowed`, 400), false);
   }
 
-  return multer({fileFilter, storage});
+  return multer({
+    fileFilter, storage, limits: {
+      fileSize: 5 * 1024 * 1024, // 5 MB per image
+      files: 10
+    }
+  });
 
 }
