2dbea619e046a5f751d7980bebc4ac22dde04575
Ososmeedo1 <153993814+Ososmeedo1@users.noreply.github.com>
2026-08-31 22:35:55 +0300

    Expand the number of images accepted for each product

--- src/Modules/Products/products.routes.js
+++ src/Modules/Products/products.routes.js
@@ -7,7 +7,7 @@ import { Brand } from "../../../DB/Models/index.js";
 
 const productRouter = Router();
 
-productRouter.post('/add', multerHost(extensions.Images).array("images", 5), auth(['Admin']), validationMiddleware(productSchema.addProduct), checkIfIdsExist(Brand), errorHandle(productController.addProduct))
+productRouter.post('/add', multerHost(extensions.Images).array("images", 10), auth(['Admin']), validationMiddleware(productSchema.addProduct), checkIfIdsExist(Brand), errorHandle(productController.addProduct))
 productRouter.put('/update/:productId', auth(['Admin']), validationMiddleware(productSchema.updateProduct), errorHandle(productController.updateProduct))
 productRouter.get('/list', auth(), validationMiddleware(productSchema.listProducts), errorHandle(productController.listProducts))
 productRouter.get('/:_id', auth(), validationMiddleware(productSchema.getAndDeleteSpecificProduct), errorHandle(productController.getSpecificProduct))
